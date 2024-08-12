5982aefd1b46d6a73af2e84c3343ead8081cc102
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2024-08-12 18:03:52 +0800

    Set directories configuration

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,13 @@ const webc = require('@11ty/eleventy-plugin-webc')
 
 module.exports = (Config) => {
     Config.addPlugin(webc)
+
+    return {
+        dir: {
+            input: '_input',
+            output: '_output',
+            includes: '../_include',
+            layouts: '../_include/layout'
+        }
+    }
 }
