faa92c50f0deb4702150f2134b885ad2936ac09f
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2024-08-14 22:26:35 +0800

    feat:Add main navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 const webc = require('@11ty/eleventy-plugin-webc')
+const navigation = require('@11ty/eleventy-navigation')
 
 const dayjs = require('dayjs')
 const advancedFormat = require('dayjs/plugin/advancedFormat')
 
 module.exports = (Config) => {
     Config.addPlugin(webc)
+    Config.addPlugin(navigation)
 
     Config.addShortcode('formatted_date', (d) => dayjs(d).format('Do MMMM YYYY - dddd'))
 
@@ -20,7 +22,8 @@ module.exports = (Config) => {
             input: '_input',
             output: '_output',
             includes: '../_includes',
-            layouts: '../_includes/layouts'
+            layouts: '../_includes/layouts',
+            components: '_components',
         }
     }
 }
