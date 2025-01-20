924faeceae8873277a228882fb8c886666c97a58
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2025-01-20 18:23:12 +0800

    Add CNAME file addPassthroughCopy for GitHub custom URL implementation

--- .eleventy.js
+++ .eleventy.js
@@ -18,6 +18,7 @@ module.exports = (Config) => {
     Config.addPassthroughCopy('images')
     Config.addPassthroughCopy('css')
     Config.addPassthroughCopy('js')
+    Config.addPassthroughCopy('CNAME')
 
     return {
         dir: {
