ded3c3b60a854d0221e9559912f987aed0c78532
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2025-01-31 07:07:49 +0800

    fix: Use Liquid templates instead of renderTemplate
    The render plugin was causing issues. See 11ty/eleventy#3641

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const webc = require('@11ty/eleventy-plugin-webc')
 const navigation = require('@11ty/eleventy-navigation')
-const { EleventyRenderPlugin } = require("@11ty/eleventy");
 
 const dayjs = require('dayjs')
 const advancedFormat = require('dayjs/plugin/advancedFormat')
@@ -10,8 +9,6 @@ dayjs.extend(advancedFormat)
 module.exports = (Config) => {
     Config.addPlugin(webc)
     Config.addPlugin(navigation)
-
-    Config.addPlugin(EleventyRenderPlugin);
 
     Config.addShortcode('formatted_date', (d) => dayjs(d).format('Do MMMM YYYY - dddd'))
     Config.addShortcode('iso_date', (d) => dayjs(d).toISOString())
